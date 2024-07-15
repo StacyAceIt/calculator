af99468325f523f8251a0bbff0267f5423e0d6e4
StacyAceIt <30737898+StacyAceIt@users.noreply.github.com>
2024-07-15 16:08:51 -0400

    added background color to body element

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
         const randomIndex = Math.floor(Math.random() * 6);
         return `url('./img/${randomIndex}.png')`;
     }
-
-    // Set the background-image of the container to a random image
     const container = document.getElementById("container");
     container.style.backgroundImage = getRandomImage();
 });
