595e119082455099abada25774164d7f7d11fd19
StacyAceIt <30737898+StacyAceIt@users.noreply.github.com>
2024-07-16 10:48:13 -0400

    finished screen of calculator

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    function getRandomImage() {
-        const randomIndex = Math.floor(Math.random() * 6);
-        return `url('./img/${randomIndex}.png')`;
-    }
-    const container = document.getElementById("container");
-    container.style.backgroundImage = getRandomImage();
-});
+function getRandomImage() {
+    const randomIndex = Math.floor(Math.random() * 6);
+    return `url('./img/${randomIndex}.png')`;
+}
+const container = document.getElementById("container");
+container.style.backgroundImage = getRandomImage();
