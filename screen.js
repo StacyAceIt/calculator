932393d6c3b2eba941485636a2be06d026287bb1
StacyAceIt <30737898+StacyAceIt@users.noreply.github.com>
2024-08-02 17:58:26 -0400

    added adjestFontSize method in screen.js

--- screen.js
+++ screen.js
@@ -3,16 +3,26 @@ export class Screen{
         this.screen = document.getElementById("screen");
     }
     concatScreenContent(text){
-        this.screen.textContent += text;
+        this.setContent(this.adjustFontSize(this.getContent() + text));
     }
     replaceScreenContent(text){
-        this.screen.textContent = text;
+        this.setContent(this.adjustFontSize(text));
     }
     reset(){
         this.screen.textContent = "0";
     }
     getContent(){
         return this.screen.textContent;
+    }
+    setContent(updatedText){
+        this.screen.textContent = updatedText;
+    }
+    adjustFontSize(text){
+        if (text.length > 10){
+            this.screen.style.fontSize = "2rem";
+
+        }
+        return text;
     }
     
 }
